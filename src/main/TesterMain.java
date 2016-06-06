af4830fb12b7aac04c3fb6f49b6e4acb5c6db9b9
trahloff <tobias.rahloff@de.ibm.com>
2016-06-06 13:52:10 +0200

    test commit protection

--- src/main/TesterMain.java
+++ src/main/TesterMain.java
@@ -5,6 +5,7 @@ public class TesterMain {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("hello peter");
+		System.out.println("hello simone");
 
 	}
 
