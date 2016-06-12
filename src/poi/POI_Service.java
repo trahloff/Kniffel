108f9a5773ebb07d72f0e45b6887056732f8539e
trahloff <tobias.rahloff@de.ibm.com>
2016-06-13 00:02:18 +0200

    minor qol changes

--- src/poi/POI_Service.java
+++ src/poi/POI_Service.java
@@ -10,7 +10,6 @@ import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 
 //@SuppressWarnings({"unused" })
